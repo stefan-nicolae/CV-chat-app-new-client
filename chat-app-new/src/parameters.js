e676b70211a287098f4ced11c8d6e601bf656ce5
stefan-nicolae <84786481+stefan-nicolae@users.noreply.github.com>
2024-02-12 19:16:53 +0200

    Update parameters.js

--- chat-app-new/src/parameters.js
+++ chat-app-new/src/parameters.js
@@ -3,6 +3,7 @@ const params = new URLSearchParams(window.location.search)
 let URLkeywords = Array.from(params.getAll('keywords'))
 URLkeywords = URLkeywords.length ? URLkeywords[0].replaceAll(" ", '').split(",") : []
 
-const chatID = params.getAll('chatID')
+const chatID = params.getAll('chatID')[0]
+console.log("chatID", chatID)
 
 export { URLkeywords, chatID }
