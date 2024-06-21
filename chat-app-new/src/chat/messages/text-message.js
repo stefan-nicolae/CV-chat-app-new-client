019dfe895bbe781abe134c564d036de031bc4b82
stefan-nicolae <84786481+stefan-nicolae@users.noreply.github.com>
2024-06-21 19:46:05 +0300

    URL opening

--- chat-app-new/src/chat/messages/text-message.js
+++ chat-app-new/src/chat/messages/text-message.js
@@ -1,18 +1,30 @@
+import React from 'react';
+
 function isValidURL(string) {
-    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    return (res !== null)
-  };
+  var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  return (res !== null);
+}
 
-  export default function TextMessage (props) {
-      const className = `message text-message ${props.me ? "me" : "them"} ${props.isClosed ? "msgClosed" : ""}`
-      if(isValidURL(props.message)) {
-        if(!props.message.startsWith("www.")) props.message = "www." + props.message
-        if(!props.message.startsWith("http://")) props.message = "http://" + props.message
+export default function TextMessage(props) {
+  const className = `message text-message ${props.me ? "me" : "them"} ${props.isClosed ? "msgClosed" : ""}`;
+
+  const words = props.message.split(" ");
+
+  const elements = words.map((word, index) => {
+    if (isValidURL(word)) {
+      let url = word;
+      if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = "http://" + url;
       }
+      return <a key={index} href={url} target="_blank" rel="noopener noreferrer">{word} </a>;
+    } else {
+      return <span key={index}>{word} </span>;
+    }
+  });
 
-    return(<div className={className}>
-        {
-            isValidURL(props.message) ?<a href={props.message} target="_blank">{props.message}</a> : props.message
-        }
-    </div>)
+  return (
+    <div className={className}>
+      {elements}
+    </div>
+  );
 }
